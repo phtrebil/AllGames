4199d303c9043c069102257ea82eab77c91d4b44
phtrebil <phptrebil@gmail.com>
2023-10-02 21:06:50 -0300

    utilizando a classe gamer para estudar classe com mais de um contrutor

---
+++ src/main/kotlin/br/com/pedro/allgames/main/TesteGamer.kt
@@ -0,0 +1,19 @@
+package br.com.pedro.allgames.main
+
+import br.com.pedro.allgames.model.Gamer
+
+fun main(){
+    val gamer1 = Gamer("Pedro", "phtrebil@gmail.com.br")
+    val gamer2 = Gamer("Jenni", "jeni@gmail.com", "19/12/1992", "jeniblo")
+
+    println(gamer1)
+    println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "12/12/1990"
+        it.usuario = "phtrebil"
+        it.idInterno= "phtrebil"
+    }
+
+    println(gamer1)
+}
