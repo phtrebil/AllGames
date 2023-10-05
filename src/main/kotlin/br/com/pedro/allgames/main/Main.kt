d49d8d6a38432256238cb43aaf9937b4310eb608
phtrebil <phptrebil@gmail.com>
2023-10-05 19:32:35 -0300

    adicionando loop na pesquisa de jogos

--- src/main/kotlin/br/com/pedro/allgames/main/Main.kt
+++ src/main/kotlin/br/com/pedro/allgames/main/Main.kt
@@ -5,46 +5,50 @@ import br.com.pedro.allgames.services.ConsumeApi
 import java.util.*
 
 fun main() {
-    val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
 
-    val buscaApi = ConsumeApi()
+    do{
+        val leitura = Scanner(System.`in`)
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
+
+        val buscaApi = ConsumeApi()
 
 
 
-    var meuJogo: Jogo? = null
+        var meuJogo: Jogo? = null
 
-    val resultado = runCatching {
-        val informacaoJogo = buscaApi.findGame(busca)
-        meuJogo = Jogo(
-            informacaoJogo.info.title,
-            informacaoJogo.info.thumb
-        )
-    }
-
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-        if (opcao.equals("s", true)) {
-            println("Insira a descrição personalizado para o jogo:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.nome
-
+        val resultado = runCatching {
+            val informacaoJogo = buscaApi.findGame(busca)
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb
+            )
         }
 
-        println(meuJogo)
-    }
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id.")
+        }
 
-    resultado.onSuccess {
-        println("Busca finalizada com sucesso.")
-    }
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição personalizado para o jogo:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.nome
 
+            }
+
+            println(meuJogo)
+
+        }
+        println("Deseja buscar um novo jogo? S/N")
+
+        val resposta = leitura.nextLine()
+    }while (resposta.equals("s", ignoreCase = true))
+
+    println("Busca finalizada!")
 
 }
--- src/main/kotlin/br/com/pedro/allgames/model/Gamer.kt
+++ src/main/kotlin/br/com/pedro/allgames/model/Gamer.kt
@@ -16,7 +16,7 @@ data class Gamer(
         }
     var idInterno: String? = null
         private set
-
+    val jogosBuscados = mutableListOf<Jogo>()
 
     constructor(nome: String, email: String, dataNascimento: String, usuario: String) : this(nome, email) {
 
