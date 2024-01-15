6bbfa68357ae0de1b6cea2ed1f64e5f8772b7b37
phtrebil <phptrebil@gmail.com>
2024-01-15 20:47:38 -0300

    implementando regra de desconto por reputação

--- src/main/kotlin/br/com/pedro/allgames/model/PlanoAvulso.kt
+++ src/main/kotlin/br/com/pedro/allgames/model/PlanoAvulso.kt
@@ -2,4 +2,13 @@ package br.com.pedro.allgames.model
 
 class PlanoAvulso(
     tipo: String
-): Plano(tipo)
+): Plano(tipo){
+    override fun obterValor(aluguel: Aluguel): Double {
+        val valorOriginal = super.obterValor(aluguel)
+        val valorComDesconto = super.obterValor(aluguel) * 0.9
+        if(aluguel.gamer.media > 8){
+            return valorComDesconto
+        }
+        return valorOriginal
+    }
+}
